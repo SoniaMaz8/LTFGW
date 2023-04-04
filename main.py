459d7426ad407b174cb48294a479b69f625fa9cb
SoniaMazelet <121769948+SoniaMaz8@users.noreply.github.com>
2023-04-04 17:41:09 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,100 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import networkx as nx
+from torch_geometric.data import Data
+from utils import plot_graph, distance_to_template, get_sbm
+from torch_geometric.utils import to_networkx
+from networkx.generators.community import stochastic_block_model as sbm
+from sklearn.manifold import MDS
+
+
+rng = np.random.RandomState(42)
+
+#toy graph
+
+n = 20
+feat_C1=np.random.randint(1,5,n) #node features
+nc = 3
+ratio = np.array([.2, .3, .5])
+P = np.array(0.6 * np.eye(3) + 0.02 * np.ones((3, 3)))
+C1 = get_sbm(n, nc, ratio, P) 
+
+# get 2d position for nodes
+x1 = MDS(dissimilarity='precomputed', random_state=0).fit_transform(1 - C1)
+
+plt.figure(1, (10, 5))
+plt.clf()
+plt.subplot(1, 2, 1)
+plot_graph(x1, C1, color='C0')
+plt.title("Toy graph")
+plt.axis("off")
+plt.subplot(1, 2, 2)
+plt.imshow(C1, interpolation='nearest')
+plt.title("Adjacency matrix")
+plt.axis("off")
+plt.show()
+
+#node features
+
+feat_C1=[0,0,0,0,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2]
+feat_C1=feat_C1+np.random.rand(20)
+ 
+#compute the templates with SBM generator
+
+
+np.random.seed(42)
+N = 10  # number of templates
+n_cluster = 2
+dataset = []
+features_templates = []
+
+#inter-intra cluster probability
+p_inter = 0.3
+p_intra = 0.6
+
+#templates generation
+for i in range(N):
+    n_nodes = int(np.random.uniform(low=5, high=7))
+    P = p_inter * np.ones((n_cluster, n_cluster))
+    np.fill_diagonal(P, p_intra)
+    sizes=np.round(n_nodes * np.ones(n_cluster) / n_cluster).astype(np.int32)
+    G = sbm(sizes, P, seed=i, directed=False)
+    C = nx.to_numpy_array(G)
+    m=C.shape[0]
+    dataset.append(C) 
+    feat=torch.tensor(np.random.randint(1,4,m),dtype=torch.long)
+    features_templates.append(feat)
+
+plt.figure(2,figsize=(13,6))
+plt.clf()
+for i in range(N):
+  C = dataset[i]
+  x = MDS(dissimilarity='precomputed', random_state=0).fit_transform(C)
+  plt.subplot(1, N, i+1)
+  plot_graph(x, C)
+  plt.title('{}'.format(i+1), fontsize=14)
+  plt.axis("off")
+plt.show()
+
+#distances to subgraphs computations
+nb_neighbours=1
+feat_C1=torch.tensor(feat_C1,dtype=torch.long)
+feat_C1=feat_C1.long()
+dist= distance_to_template(C1,feat_C1,dataset,features_templates,nb_neighbours)
+
+plt.figure(3)
+plt.imshow(dist)
+plt.title('distance matrix')
+plt.xlabel('templates')
+plt.ylabel('nodes')
+plt.show()
+
+#visualisation of the embeddings along the 2 first dimensions
+plt.figure(4)
+plt.scatter(dist[:4,0],dist[:4,1],color='r',label='class 1')
+plt.scatter(dist[4:10,0],dist[4:10,1],color='b',label='class 2')
+plt.scatter(dist[10:20,0],dist[10:20,1],color='g',label='class 3')
+plt.title('visualisation of the embeddings along the 2 first dimensions')
+plt.legend()
+plt.show()
---
+++ utils.py
@@ -0,0 +1,134 @@
+import numpy as np
+import torch
+import os
+import matplotlib.pyplot as plt
+import networkx as nx
+from torch_geometric.data import Data
+from torch_geometric.utils import k_hop_subgraph
+import ot
+
+
+rng = np.random.RandomState(42)
+
+def visualize_graph(G, color='b'):
+    """"
+    visualisation of a torch.geometric graph
+    """
+    plt.figure(figsize=(7,7))
+    plt.xticks([])
+    plt.yticks([])
+    nx.draw_networkx(G, pos=nx.spring_layout(G, seed=42), with_labels=True,
+                     node_color=color, cmap="Set2")
+    plt.show()
+
+def plot_graph(x, C, binary=True, color='C0', s=None):
+    for j in range(C.shape[0]):
+        for i in range(j):
+            if binary:
+                if C[i, j] > 0:
+                    plt.plot([x[i, 0], x[j, 0]], [x[i, 1], x[j, 1]], alpha=0.2, color='k')
+            else:  # connection intensity proportional to C[i,j]
+                plt.plot([x[i, 0], x[j, 0]], [x[i, 1], x[j, 1]], alpha=C[i, j], color='k')
+
+    plt.scatter(x[:, 0], x[:, 1], c=color, s=s, zorder=10, edgecolors='k', cmap='tab10', vmax=9)
+
+def graph_to_adjacency(nodes,edges):
+    """"
+    adjacency matrix of a graph given its nodes and edges in a torch.geometric format
+    """
+    n=len(nodes)
+    C=np.zeros((n,n))
+    m=len(edges[0])
+    for i in range(m):
+        C[edges[0,i],edges[1,i]]=1
+    return C
+
+def get_sbm(n, nc, ratio, P):
+    nbpc = np.round(n * ratio).astype(int)
+    n = np.sum(nbpc)
+    C = np.zeros((n, n))
+    for c1 in range(nc):
+        for c2 in range(c1 + 1):
+            if c1 == c2:
+                for i in range(np.sum(nbpc[:c1]), np.sum(nbpc[:c1 + 1])):
+                    for j in range(np.sum(nbpc[:c2]), i):
+                        if rng.rand() <= P[c1, c2]:
+                            C[i, j] = 1
+            else:
+                for i in range(np.sum(nbpc[:c1]), np.sum(nbpc[:c1 + 1])):
+                    for j in range(np.sum(nbpc[:c2]), np.sum(nbpc[:c2 + 1])):
+                        if rng.rand() <= P[c1, c2]:
+                            C[i, j] = 1
+
+    return C + C.T
+
+
+def adjacency_to_graph(C,x):
+    """
+    Returns a torch_geometric graph given binary adjacency matrix and features
+    C : adjacency matrix of the graph
+    x : features of the nodes of the graph
+    """
+    n=C.shape[0]
+    senders=[]
+    receivers=[]
+    for i in range(n):
+        for j in range(n):
+            if C[i,j]>0:
+                senders.append(i)
+                receivers.append(j)
+    edge_index=np.zeros((2,len(senders)))
+    edge_index[0]=senders
+    edge_index[1]=receivers
+    edge_index=torch.tensor(edge_index,dtype=torch.long)
+    x=torch.tensor(x,dtype=torch.float)
+    return Data(x=x,edge_index=edge_index)
+
+def subgraph(C,x,node_idx, order):
+    """
+    Computes the edges and nodes of a subgraph center at node_idx of order k
+    C : adjacency matrix of the graph
+    x : features of the nodes of the graph
+    node_idx : index of the node to center the subgraph
+    order : order of te subgraph
+    """
+    G=adjacency_to_graph(C,x)
+    sub_G=k_hop_subgraph(node_idx,order,edge_index=G.edge_index,relabel_nodes=False) 
+    x_sub=x[sub_G[0]]
+    sub_G=k_hop_subgraph(node_idx,order,edge_index=G.edge_index,relabel_nodes=True) #surement une meilleure manière de faire
+    edges_sub=sub_G[1]
+    C_sub=graph_to_adjacency(sub_G[0],edges_sub)
+    return C_sub,x_sub
+
+
+
+def distance_to_template(C,x_C,T,x_T,k):
+    """
+    Computes the OT distance between each subgraphs of order k of G and the templates
+    C : adjacency matrix of the graph
+    x_C : features of the nodes of the graph
+    T : list of adjacency matrices of the templates
+    x_T : list of the features of the templates nodes
+    k : number of neighbours in the subgraphs
+    """
+    n=C.shape[0]
+    n_T=len(T)
+    distances=np.zeros((n,n_T))
+
+    for i in range(n):
+        for j in range(n_T):
+          
+          C_sub,x_sub=subgraph(C,x_C,i,k)
+          x_sub=x_sub.reshape(len(x_sub),1)  #reshape pour utiliser ot.dist
+          template_features=x_T[j].reshape(len(x_T[j]),1)   #reshape pour utiliser ot.dist
+          M=np.array(ot.dist(x_sub,template_features))  #cost matrix between the features of the subgraph and the template
+
+          n_sub=len(x_sub)
+          n_template=len(x_T[j]) 
+          p=np.ones(n_sub)/n_sub
+          q=np.ones(n_template)/n_template
+          
+          dist=ot.gromov.fused_gromov_wasserstein2(M, C_sub, T[j], p, q)
+          distances[i,j]=dist
+
+    return distances
