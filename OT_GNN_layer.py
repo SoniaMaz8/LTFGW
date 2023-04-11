baee45900afd5ac2687b4d5940aaa7def6695d7a
SoniaMazelet <121769948+SoniaMaz8@users.noreply.github.com>
2023-04-11 10:25:02 +0200

    Add files via upload

---
+++ OT_GNN_layer.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+from utils import distance_to_template,graph_to_adjacency, adjacency_to_graph
+from torch_geometric.data import Data as GraphData
+from torch_geometric.nn import GCNConv, Linear
+
+dataset=torch.load('toy_graph1.pt')
+
+class OT_GNN_layer(nn.Module):
+    def __init__(self,N_templates=10,N_templates_nodes=10):
+        """
+        N_templates: number of templates
+        N_templates_nodes: number of nodes in each template
+        """
+        super().__init__()
+        self.N_templates=N_templates
+        self.N_templates_nodes=N_templates_nodes
+
+        templates=torch.Tensor(N_templates,N_templates_nodes,N_templates_nodes)  #templates adjacency matrices 
+        self.templates = nn.Parameter(templates)
+
+        templates_features=torch.Tensor(N_templates,N_templates_nodes,dataset.num_features)
+        self.templates_features = nn.Parameter(templates_features)
+
+       # self.conv= GCNConv(self.N_templates, dataset.num_classes)
+        self.linear=Linear(self.N_templates, dataset.num_classes)
+
+        # initialize adjacency matrices for the templates
+        nn.init.uniform_(self.templates)
+        nn.init.uniform_(self.templates_features)
+
+    def forward(self, G):
+        C=graph_to_adjacency(G.num_nodes,G.edge_index)
+        F_C=distance_to_template(C,G.x,self.templates,self.templates_features,1,G.num_features)
+        x=self.linear(F_C)
+       # x=self.conv(F_C,G.edge_index)
+        return  x
+    
+
+torch.manual_seed(0)
+model=OT_GNN_layer()
+
+criterion = torch.nn.CrossEntropyLoss()  
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+def train():
+      model.train()
+      optimizer.zero_grad()  # Clear gradients.
+      out = model(dataset)  # Perform a single forward pass.
+      loss = criterion(out[dataset.train_mask], dataset.y[dataset.train_mask])  # Compute the loss solely based on the training nodes.
+      loss.backward()  # Derive gradients.
+      optimizer.step()  # Update parameters based on gradients.
+      return loss
+
+def test():
+      model.eval()
+      out = model(dataset)
+      print(out)
+      pred = out.argmax(dim=1)  # Use the class with highest probability.
+      print(pred)
+      test_correct = pred[dataset.test_mask] == dataset.y[dataset.test_mask]  # Check against ground-truth labels.
+      test_acc = int(test_correct.sum()) / int(dataset.test_mask.sum())  # Derive ratio of correct predictions.
+      return test_acc
+
+for epoch in range(1, 10):
+    loss = train()
+    print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}')
+
+test_acc = test()
+print(f'Test Accuracy: {test_acc:.4f}')   
+
+
