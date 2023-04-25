acf8953e285dcbbf10ce78b9c811036fdf42d9c7
SoniaMazelet <121769948+SoniaMaz8@users.noreply.github.com>
2023-04-25 09:53:15 +0200

    Update train_toy_graph.py

--- train_toy_graph.py
+++ train_toy_graph.py
@@ -11,7 +11,7 @@ criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
 
 
-def train_epoch(dataset):
+def train_epoch(dataset, save=False):
       ''''
       save: bool, wether to save the parameters after each epoch or not
       '''
@@ -21,6 +21,9 @@ def train_epoch(dataset):
       pred = out.argmax(dim=1)         # Use the class with highest probability
       train_correct = pred[dataset.train_mask] == dataset.y[dataset.train_mask]   #number of correct node predictions
       train_acc = int(train_correct.sum()) / int(dataset.train_mask.sum())  #training_accuracy
+      if save:
+
+            
       loss = criterion(out[dataset.train_mask], dataset.y[dataset.train_mask])  
       loss.backward()  
       optimizer.step()  
@@ -28,27 +31,35 @@ def train_epoch(dataset):
 
 #acces csv file to save the results
 
-now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-new_column_name = f'Loss/train_accuracy_{now}'
-filename = 'loss_history.csv'
-with open(filename, 'a', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow([new_column_name])
+
  
 #train
 
-for epoch in range(50):
-     loss,train_acc = train_epoch(dataset)
-     with open(filename, 'a', newline='') as f:
+def train(dataset,N_epoch,save):
+      ''''
+      save: bool, wether to save the parameters after each epoch or not
+      '''
+      if save:
+            now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            new_column_name = f'Loss/train_accuracy_{now}'
+            filename = 'loss_history.csv'
+            with open(filename, 'a', newline='') as f:
             writer = csv.writer(f)
-            writer.writerow([epoch,loss.item(),train_acc])
-     print(f'Epoch: {epoch:03d}, Loss: {loss:.4f},Train Accuracy: {train_acc:.4f}')
-  
-
-   
-
-torch.save({
+            writer.writerow([new_column_name]) 
+      for epoch in range(50):      
+            loss,train_acc = train_epoch(dataset)
+            if save:
+                  with open(filename, 'a', newline='') as f:
+                        writer = csv.writer(f)
+                        writer.writerow([epoch,loss.item(),train_acc])  
+                        
+            print(f'Epoch: {epoch:03d}, Loss: {loss:.4f},Train Accuracy: {train_acc:.4f}')     
+      if save:
+            torch.save({
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
-            }, 'model_Citeseer.pt')
+            }, 'model_toy.pt')
+       
+
+
   
