7c135fb3fa847e046daa4ea06117b3bf00fa4abb
SoniaMazelet <121769948+SoniaMaz8@users.noreply.github.com>
2023-04-25 10:34:35 +0200

    Add files via upload

---
+++ data/convert_datasets.py
@@ -0,0 +1,18 @@
+import dgl 
+import torch
+from torch_geometric.data import Data as GraphData
+
+
+
+def Citeseer_data():
+  dataset=dgl.data.CiteseerGraphDataset()
+  g = dataset[0]
+  features=g.ndata['feat']
+  edges=g.edges()
+  n=len(edges[0])
+  edges=torch.cat((edges[0],edges[1]))
+  edges=edges.reshape(2,n)
+  label = g.ndata['label']
+  n_feat=len(features[0])
+  G=GraphData(x=features, edge_index=edges,y=label, num_features=n_feat , num_classes=6,train_mask = g.ndata['train_mask'],test_mask = g.ndata['test_mask'])
+  return(G)
