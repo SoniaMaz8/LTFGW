2519e76c709076ab4cc3ca17a97ad99fe844f42c
Sonia188 <54401007+Sonia188@users.noreply.github.com>
2023-04-26 16:15:12 +0200

    save pandas

--- run.py
+++ run.py
@@ -1,27 +1,28 @@
 from torch_geometric.loader import NeighborLoader
 from architectures import GCN_LTFGW, GCN_3_layers
 import torch
-from torch.optim.lr_scheduler import MultiStepLR
 from data.convert_datasets import Citeseer_data
 from trainers import train,train_minibatch, test
-from tqdm import tqdm
-import torch.nn.functional as F
 import os
 import pandas as pd
 
 torch.manual_seed(123456)
 
+#%%Parameters to set
+
 dataset_name='citeseer'  #'citeseer' or 'toy_graph'
-model_name='GCN'  #'LTFGW' or 'GCN'
+model_name='LTFGW'  #'LTFGW' or 'GCN'
 save_parameters=True  #wether to save the parameters and the model
 N_epoch=50   #number of epochs
-training='complete_graph'     #'complete graph' or 'minibatch' 
-lr=0.01
+training='mini_batch'     #'complete graph' or 'mini_batch' 
+lr=0.01  #learning rate
 weight_decay=5e-4
 
 criterion = torch.nn.CrossEntropyLoss() 
 
-num_seeds=0   
+num_seeds=0   #number of different seeds to train with
+
+#%%Training and testing
 
 Test_accuracy=0
 seeds=torch.range(30,30+num_seeds,1)
@@ -31,24 +32,20 @@ for seed in seeds:
     if dataset_name=='citeseer':
         dataset=Citeseer_data()
         n_classes=6
-        filename_values='results/Citeseer'
-        filename_model='models/Citeseer'
+        filename_save='results/Citeseer'
 
     elif dataset_name=='toy_graph':
         dataset=torch.load('data/toy_graph1.pt')
         n_classes=3
-        filename_values='results/toy_graph'
-        filename_model='models/toy_graph'    
+        filename_save='results/toy_graph'  
 
     if model_name=='LTFGW':
         model=GCN_LTFGW(n_classes=n_classes,N_features=dataset.num_features, N_templates=10,N_templates_nodes=10)
-        filename_values=os.path.join(filename_values,'LTFGW.csv')
-        filename_model=os.path.join(filename_model,'LTFGW.pt')
+        filename_save=os.path.join( filename_save,'LTFGW.csv')
 
     elif model_name=='GCN':
         model=GCN_3_layers(n_classes=n_classes,N_features=dataset.num_features,dropout=0.6)
-        filename_values=os.path.join(filename_values,'GCN.csv')
-        filename_model=os.path.join(filename_model,'GCN.pt')
+        filename_save=os.path.join(filename_save,'GCN.csv')
     
     optimizer=torch.optim.Adam(model.parameters(), lr=lr,weight_decay=weight_decay)
 
@@ -56,24 +53,26 @@ for seed in seeds:
         train_loader = NeighborLoader(dataset,num_neighbors= [-1],
         batch_size=8,
         input_nodes=dataset.train_mask,shuffle=True)
-        Loss, Train_acc=train_minibatch(model,train_loader,dataset,optimizer,criterion,N_epoch,save_parameters,filename_values,filename_model)
+        Loss, Train_acc=train_minibatch(model,train_loader,dataset,optimizer,criterion,N_epoch)
         
     elif training=='complete_graph':
-        Loss, Train_acc=train(model,dataset,N_epoch,criterion, optimizer,save_parameters,filename_values,filename_model)
+        Loss, Train_acc=train(model,dataset,N_epoch,criterion, optimizer)
         
     test_acc=test(model,dataset)
     Test_accuracy+=test_acc
 
     if save_parameters:
-        df = pd.read_csv(filename_values)
+        print(filename_save)
+        df = pd.read_csv(filename_save)
         print(df)
-        new_row={'seed':seed.item(), 'loss': Loss,'train_accuracy':Train_acc ,'test_accuracy':test_acc}
+        new_row={'seed':seed.item(), 'loss': Loss,'train_accuracy':Train_acc ,'test_accuracy':test_acc,'model_parameters': model.state_dict()}
         df.loc[len(df)]=new_row
-        df.to_csv(filename_values)
+        df.to_csv(filename_save)
 
 
 #print the average of the test accuracies over the seeds
-print('test_accuracy={}'.format( Test_accuracy/len(seeds)))        
+
+print('mean_test_accuracy={}'.format( Test_accuracy/len(seeds)))        
     
 
 
--- trainers.py
+++ trainers.py
@@ -1,25 +1,25 @@
-import torch
 import time
 from tqdm import tqdm
-import csv
-import datetime
-import torch.nn.functional as F
-from architectures import GCN_3_layers
-import pandas as pd
 
 def train_epoch(dataset,model,criterion,optimizer):
-      model.train()
-      optimizer.zero_grad()  
-      out = model(dataset.x,dataset.edge_index)
-      pred = out.argmax(dim=1)         # Use the class with highest probability
-      train_correct = pred[dataset.train_mask] == dataset.y[dataset.train_mask]   #number of correct node predictions
-      train_acc = int(train_correct.sum()) / int(dataset.train_mask.sum())  #training_accuracy
-      loss = criterion(out[dataset.train_mask], dataset.y[dataset.train_mask])  
-      loss.backward()  
-      optimizer.step()  
-      return loss, train_acc
+    """"
+    train one epoch on the complete graph
+    """
+    model.train()
+    optimizer.zero_grad()  
+    out = model(dataset.x,dataset.edge_index)
+    pred = out.argmax(dim=1)         # Use the class with highest probability
+    train_correct = pred[dataset.train_mask] == dataset.y[dataset.train_mask]   #number of correct node predictions
+    train_acc = int(train_correct.sum()) / int(dataset.train_mask.sum())  #training_accuracy
+    loss = criterion(out[dataset.train_mask], dataset.y[dataset.train_mask])  
+    loss.backward()  
+    optimizer.step()  
+    return loss, train_acc
 
 def train_epoch_minibatch(model,train_loader,data,optimizer,criterion):
+    """"
+    train one epoch with minibatches
+    """    
     model.train()
     total_loss = 0
     total_train_acc=0
@@ -37,44 +37,45 @@ def train_epoch_minibatch(model,train_loader,data,optimizer,criterion):
     return total_loss / len(train_loader), total_train_acc / len(train_loader)  #mean of train accuracy and loss over the mini batches
 
 
-def train_minibatch(model,train_loader,dataset,optimizer,criterion,N_epoch,save,filename_values,filename_model):
-      ''''
-      save: bool, wether to save the parameters after each epoch or not
-      filename_values: filename to store the loss and train accuracies
-      flename_model: filename to store the model parameters
-      '''
-      Loss=[]
-      Train_acc=[]      
-      for epoch in range(N_epoch):  
-            start=time.time()      
-            loss,train_acc = train_epoch_minibatch(model,train_loader,dataset,optimizer,criterion)
-            Loss.append(loss.item())
-            Train_acc.append(train_acc)            
-            end=time.time()   
-            print(f'Epoch: {epoch:03d},time:{end-start:.4f}, Loss: {loss:.4f},Train Accuracy: {train_acc:.4f}')     
-      return Loss, Train_acc
+def train_minibatch(model,train_loader,dataset,optimizer,criterion,N_epoch):
+    """"
+    train the entire model with minibatches
+    """        
+    Loss=[]
+    Train_acc=[]      
+    for epoch in range(N_epoch):  
+        start=time.time()      
+        loss,train_acc = train_epoch_minibatch(model,train_loader,dataset,optimizer,criterion)
+        Loss.append(loss)
+        Train_acc.append(train_acc)            
+        end=time.time()   
+        print(f'Epoch: {epoch:03d},time:{end-start:.4f}, Loss: {loss:.4f},Train Accuracy: {train_acc:.4f}')     
+    return Loss, Train_acc
 
 
-def train(model,dataset,N_epoch,criterion, optimizer,save,filename_values,filename_model):
-      ''''
-      save: bool, wether to save the parameters after each epoch or not
-      '''
-      Loss=[]
-      Train_acc=[]
-      for epoch in range(N_epoch): 
+def train(model,dataset,N_epoch,criterion, optimizer):
+    """"
+    train the entire model on the entire graph
+    """         
+    Loss=[]
+    Train_acc=[]
+    for epoch in range(N_epoch): 
             start=time.time()     
             loss,train_acc = train_epoch(dataset,model,criterion,optimizer)
             end=time.time()
             Loss.append(loss.item())
             Train_acc.append(train_acc)
             print(f'Epoch: {epoch:03d},time:{end-start:.4f}, Loss: {loss:.4f},Train Accuracy: {train_acc:.4f}')     
-      return Loss, Train_acc
+    return Loss, Train_acc
 
 def test(model,dataset):
-      model.eval()
-      out = model(dataset.x,dataset.edge_index)
-      pred = out.argmax(dim=1)  # Use the class with highest probability.
-      test_correct = pred[dataset.test_mask] == dataset.y[dataset.test_mask]  
-      test_acc = int(test_correct.sum()) / int(dataset.test_mask.sum()) 
-      return test_acc
+    """"
+    test the model
+    """      
+    model.eval()
+    out = model(dataset.x,dataset.edge_index)
+    pred = out.argmax(dim=1)  # Use the class with highest probability.
+    test_correct = pred[dataset.test_mask] == dataset.y[dataset.test_mask]  
+    test_acc = int(test_correct.sum()) / int(dataset.test_mask.sum()) 
+    return test_acc
 
