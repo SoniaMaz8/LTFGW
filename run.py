6efab526ef03affbe63abdf2bd2f0ff1ef06a372
Sonia188 <sonia.mazelet@gmail.com>
2023-05-05 15:41:44 +0200

    multi graph

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from torch_geometric.loader import NeighborLoader
+from torch_geometric.loader import NeighborLoader, DataLoader
 from architectures import GCN_LTFGW, GCN, MLP,LTFGW_GCN
 from utils import get_dataset, get_filenames
-from trainers import train,train_minibatch, test
+from trainers import train, test, train_multi_graph
 import os
 import pandas as pd
 import torch
@@ -10,11 +10,11 @@ torch.manual_seed(123456)
 
 #%%Parameters to set
 
-dataset_name='Toy_graph'  #'Citeseer' or 'Toy_graph'
-model_name='MLP'  # 'GCN', 'GCN_LTFGW', 'LTFGW_GCN' or 'MLP'
+dataset_name='Toy_graph_multi'  #'Citeseer' or 'Toy_graph_single' or 'Toy_graph_multi'
+model_name='LTFGW_GCN'  # 'GCN', 'GCN_LTFGW', 'LTFGW_GCN' or 'MLP'
 save=True  #wether to save the parameters and the model
-N_epoch=300 #number of epochs
-training='complete_graph'     #'complete graph' or 'mini_batch' 
+n_epoch=300 #number of epochs
+training='multi_graph'     #'complete graph' or 'multi_graph' or 'mini_batch'
 lr=0.01  #learning rate
 weight_decay=5e-4
 
@@ -31,20 +31,20 @@ for seed in seeds:
     torch.manual_seed(seed)
     
     # load dataset
-    dataset, n_classes=get_dataset(dataset_name)
+    dataset, n_classes, n_features=get_dataset(dataset_name)
 
     # init model
     if model_name=='LTFGW_GCN':
-        model=LTFGW_GCN(n_classes=n_classes,n_features=dataset.num_features, n_templates=3,n_templates_nodes=3)
+        model=LTFGW_GCN(n_classes=n_classes,n_features=n_features, n_templates=3,n_templates_nodes=3)
 
     elif model_name=='GCN_LTFGW':
-        model=GCN_LTFGW(n_classes=n_classes,n_features=dataset.num_features, n_templates=6,n_templates_nodes=6, skip_connection=True)
+        model=GCN_LTFGW(n_classes=n_classes,n_features=n_features, n_templates=6,n_templates_nodes=6, skip_connection=True)
   
     elif model_name=='MLP':
-        model=MLP(n_classes=n_classes,n_features=dataset.num_features)
+        model=MLP(n_classes=n_classes,n_features=n_features)
 
     elif model_name=='GCN':
-        model=GCN(n_classes=n_classes,n_features=dataset.num_features)
+        model=GCN(n_classes=n_classes,n_features=n_features)
 
     method=model_name+'_'+training
     filename_save, filename_best_model = get_filenames(dataset_name,method,seed)
@@ -52,25 +52,20 @@ for seed in seeds:
     optimizer=torch.optim.Adam(model.parameters(), lr=lr,weight_decay=weight_decay)
 
     best_val_perf=0
-   # if save:
-   #     df=pd.read_pickle(filename_save)
-   #     best_val_perf=df['max_val_accuracy'].max()
 
-    if training=='mini_batch':
-        train_loader = NeighborLoader(dataset,num_neighbors= [-1],
-        batch_size=8,
-        input_nodes=dataset.train_mask,shuffle=True)
-        Loss, Train_acc,Val_acc=train_minibatch(model,train_loader,dataset,optimizer,criterion,N_epoch,save,filename_save,filename_best_model,best_val_perf)
+ #   if training=='mini_batch':
+ #       train_loader = NeighborLoader(dataset,num_neighbors= [-1],
+ #       batch_size=8,
+ #       input_nodes=dataset.train_mask,shuffle=True)
+ #       train_minibatch(model,train_loader,dataset,optimizer,criterion,n_epoch,save,filename_save,filename_best_model,best_val_perf)
         
-    elif training=='complete_graph':
-        Loss, Train_acc, Val_acc=train(model,dataset,N_epoch,criterion, optimizer,save,filename_save,filename_best_model,best_val_perf)
-        
-    test_acc=test(model,dataset)
-    Test_accuracy+=test_acc
+    if training=='complete_graph':
+        train(model,dataset,n_epoch,criterion, optimizer,save,filename_save,filename_best_model,best_val_perf)
 
-#print the average of the test accuracies over the seeds
-
-print('mean_test_accuracy={}'.format( Test_accuracy/len(seeds)))        
-    
-
+    elif training=='multi_graph':
+        generator = torch.Generator().manual_seed(seed.item())
+        train_dataset,val_dataset,test_dataset=torch.utils.data.random_split(dataset,[600,200,200],generator=generator)
+        train_loader=DataLoader(train_dataset,batch_size=100,shuffle=True)
+        val_loader=DataLoader(val_dataset,batch_size=100,shuffle=True)
+        train_multi_graph(model,criterion,optimizer,n_epoch,save,filename_save,filename_best_model,train_loader,val_loader)
 
--- trainers.py
+++ trainers.py
@@ -4,6 +4,7 @@ import torch
 import os 
 import pandas as pd
 from sklearn.manifold import TSNE
+from torch_geometric.loader import DataLoader
 
 
 
@@ -14,86 +15,33 @@ def train_epoch(dataset,model,criterion,optimizer,epoch):
     model.train()
     optimizer.zero_grad()  
     out,x_latent = model(dataset.x,dataset.edge_index)
+
+    #save for visualisation
     if epoch%20==0:
        x_latent=x_latent.detach().numpy()
        df_x=pd.DataFrame(x_latent)
        df_x.to_csv('results/TSNE_{}/latent{}.csv'.format(str(model),epoch))
-    pred = out.argmax(dim=1)         # Use the class with highest probability
+
+    pred = out.argmax(dim=1)   
+
+    #train    
     train_correct = pred[dataset.train_mask] == dataset.y[dataset.train_mask]   #number of correct node predictions
     train_acc = int(train_correct.sum()) / int(dataset.train_mask.sum())  #training_accuracy
+
+    #validation
     val_correct=pred[dataset.val_mask] == dataset.y[dataset.val_mask]
     val_acc= int(val_correct.sum()) / int(dataset.val_mask.sum())
+
     loss = criterion(out[dataset.train_mask], dataset.y[dataset.train_mask])  
     loss.backward()  
     optimizer.step()  
     return loss, train_acc, val_acc
 
-def train_epoch_minibatch(model,train_loader,data,optimizer,criterion):
-    """"
-    train one epoch with minibatches
-    """    
-    model.train()
-    total_loss = 0
-    total_train_acc=0
-    total_val_acc=0
-    for data in tqdm(train_loader):
-        optimizer.zero_grad()
-        out = model(data.x,data.edge_index) 
-        pred = out.argmax(dim=1)   #predcit the class with the highest probability
-        train_correct = pred[data.train_mask] == data.y[data.train_mask]    #number of correct node predictions
-        train_acc = int(train_correct.sum()) / int(data.train_mask.sum())  #train accuracy
-        total_train_acc+=train_acc
-        val_correct=pred[data.val_mask] == data.y[data.val_mask]
-        val_acc= int(val_correct.sum()) / int(data.val_mask.sum())  
-        total_val_acc+=val_acc
-        loss = criterion(out[data.train_mask], data.y[data.train_mask])
-        total_loss += loss.item() 
-        loss.backward()
-        optimizer.step()
-    return total_loss / len(train_loader), total_train_acc / len(train_loader),  total_val_acc / len(train_loader) #mean of train accuracy and loss over the mini batches
-
-
-def train_minibatch(model,train_loader,dataset,optimizer,criterion,N_epoch,save,filename_save,filename_best_model,best_val_perf):
-    """"
-    train the entire model with minibatches
-    """        
-    Loss=[]
-    Train_acc=[] 
-    Val_acc=[]  
-    if save:
-      df=pd.DataFrame(columns=['loss','train_accuracy','validation_accuracy','test_accuracy','best_validation_accuracy'])           
-    for epoch in range(N_epoch):  
-        start=time.time()      
-        loss,train_acc, val_acc = train_epoch_minibatch(model,train_loader,dataset,optimizer,criterion)
-        Loss.append(loss)
-        Train_acc.append(train_acc)  
-        Val_acc.append(val_acc) 
-        end=time.time()  
-        if save: 
-            df.at[len(df)-1,'loss']=Loss
-            df.at[len(df)-1,'train_accuracy']=Train_acc
-            df.at[len(df)-1,'validation_accuracy']=Val_acc
-            if val_acc>best_val_perf:  
-                torch.save(model.state_dict(),filename_best_model)
-                best_val_perf=val_acc
-                df.at[len(df)-1,'best_validation_accuracy']=val_acc
-            df.to_pickle(filename_save) 
-        print(f'Epoch: {epoch:03d},time:{end-start:.4f}, Loss: {loss:.4f},Train Accuracy: {train_acc:.4f},Validation Accuracy:{val_acc:.4f}')
-    if save:
-        df = pd.read_pickle(filename_save)
-        max_val=max(df.iloc[len(df)-1]['validation_accuracy'])
-        df.at[len(df)-1,'max_val_accuracy']=max_val
-        df.to_pickle(filename_save)  
-    return Loss, Train_acc, Val_acc
-
-
 def train(model,dataset,N_epoch,criterion, optimizer,save,filename_save,filename_best_model,best_val_perf):
     """"
     train the entire model on the entire graph
     """         
-    Loss=[]
-    Train_acc=[]
-    Val_acc=[]
+
     if save:
       df=pd.DataFrame(columns=['loss','train_accuracy','validation_accuracy','test_accuracy','best_validation_accuracy']) 
       df.to_pickle(filename_save)    
@@ -112,7 +60,7 @@ def train(model,dataset,N_epoch,criterion, optimizer,save,filename_save,filename
                     df.at[epoch,'best_validation_accuracy']=val_acc
                 df.to_pickle(filename_save)        
             print(f'Epoch: {epoch:03d},time:{end-start:.4f}, Loss: {loss:.4f},Train Accuracy: {train_acc:.4f},Validation Accuracy:{val_acc:.4f}')  
-    return Loss, Train_acc, Val_acc
+
 
 def test(model,dataset):
     """"
@@ -125,3 +73,80 @@ def test(model,dataset):
     test_acc = int(test_correct.sum())/ 1000
     return test_acc
 
+
+def train_epoch_multi_graph(model,criterion,optimizer,train_loader):
+
+    model.train()
+    optimizer.zero_grad()  
+    Loss=[]
+    Train_acc=[]
+    for data in train_loader:  # Iterate in batches over the training dataset.
+           out,_ = model(data.x, data.edge_index)  # Perform a single forward pass.
+           pred=out.argmax(dim=1) 
+
+           train_correct = pred == data.y   #number of correct node predictions
+           train_acc = int(train_correct.sum()) / int(len( data.y))  #training_accuracy
+            
+           loss = criterion(out, data.y)
+
+           loss.backward()  
+           optimizer.step() 
+           optimizer.zero_grad() 
+
+           Loss.append(loss)
+           Train_acc.append(train_acc)
+
+    Loss=torch.Tensor(Loss)
+    Train_acc=torch.Tensor(Train_acc)
+
+    return torch.mean(Loss), torch.mean(Train_acc)
+
+
+def validation_epoch_multi_graph(model,val_loader):
+    model.eval() 
+    Train_acc=[]
+    for data in val_loader:  # Iterate in batches over the training dataset.
+           out,_ = model(data.x, data.edge_index)  # Perform a single forward pass.
+           pred=out.argmax(dim=1) 
+
+           val_correct = pred == data.y   #number of correct node predictions
+           val_acc = int(val_correct.sum()) / int(len( data.y))  #training_accuracy
+           Train_acc.append(val_acc)
+
+    Train_acc=torch.Tensor(Train_acc)
+
+    return torch.mean(Train_acc)
+
+
+
+def train_multi_graph(model,criterion,optimizer,n_epoch,save,filename_save,filename_best_model,train_loader,val_loader):
+
+    best_val_perf=0
+    if save:
+      df=pd.DataFrame(columns=['loss','train_accuracy','validation_accuracy','test_accuracy','best_validation_accuracy']) 
+      df.to_pickle(filename_save)  
+
+    for epoch in range(n_epoch):
+      
+      #training
+      start=time.time()
+      loss,train_acc=train_epoch_multi_graph(model,criterion,optimizer,train_loader)
+      end=time.time()
+
+      #validation
+      val_acc=validation_epoch_multi_graph(model,val_loader)
+
+      print(f'Epoch: {epoch:03d},time:{end-start:.4f}, Loss: {loss:.4f},Train Accuracy: {train_acc:.4f},Validation Accuracy:{val_acc:.4f}') 
+
+      if save: 
+        df.at[epoch,'loss']=loss.item()
+        df.at[epoch,'train_accuracy']=train_acc
+        df.at[epoch,'validation_accuracy']=val_acc
+        if val_acc>best_val_perf:  
+            torch.save({'model_state_dict':model.state_dict(),'optimizer_state_dict': optimizer.state_dict()},filename_best_model)
+            best_val_perf=val_acc
+            df.at[epoch,'best_validation_accuracy']=val_acc
+            df.to_pickle(filename_save)        
+       
+
+    
