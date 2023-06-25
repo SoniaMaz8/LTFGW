0afab1617ad806f95d5814da180d75f791f42503
sonia.mazelet <sonia.mazelet@ens-paris-saclay.fr>
2023-06-25 14:53:42 +0200

    perfs

--- job.sh
+++ job.sh
@@ -2,10 +2,10 @@
 #
 ## BEGIN SBATCH directives
 #SBATCH --job-name=OTGNN
-#SBATCH --output=res_seq9.txt
+#SBATCH --output=res_seq3.txt
 #
 #SBATCH --ntasks=1
-#SBATCH --time=35:00:00
+#SBATCH --time=40:00:00
 #SBATCH --mem=15GB
 #SBATCH --partition=cpu_shared
 #SBATCH --account=oatmil
@@ -18,4 +18,4 @@ module purge
 module load anaconda3/2020.11
 
 ## Execution
-python run.py -dropout 0.8 -model LTFGW_MLP_dropout_relu -number_of_seeds 10 -nepochs 1500 -dataset cornell_directed
+python run.py -dropout 0.8 -nepochs 1500 -lr 0.05 -n_templates_nodes 4 -n_templates 1 -number_of_seeds 10 -model LTFGW_MLP_dropout
